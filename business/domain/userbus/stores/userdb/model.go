1d3ec520a93300191f459b88326c49688792a3f0
William Kennedy <bill@ardanlabs.com>
2024-04-29 13:23:45 -0400

    commit 41

--- business/domain/userbus/stores/userdb/model.go
+++ business/domain/userbus/stores/userdb/model.go
@@ -2,9 +2,12 @@ package userdb
 
 import (
 	"database/sql"
+	"fmt"
+	"net/mail"
 	"time"
 
 	"github.com/ardanlabs/service/business/api/sqldb/dbarray"
+	"github.com/ardanlabs/service/business/domain/userbus"
 	"github.com/google/uuid"
 )
 
@@ -18,4 +21,69 @@ type user struct {
 	Enabled      bool           `db:"enabled"`
 	DateCreated  time.Time      `db:"date_created"`
 	DateUpdated  time.Time      `db:"date_updated"`
+}
+
+func toDBUser(usr userbus.User) user {
+	roles := make([]string, len(usr.Roles))
+	for i, role := range usr.Roles {
+		roles[i] = role.Name()
+	}
+
+	return user{
+		ID:           usr.ID,
+		Name:         usr.Name,
+		Email:        usr.Email.Address,
+		Roles:        roles,
+		PasswordHash: usr.PasswordHash,
+		Department: sql.NullString{
+			String: usr.Department,
+			Valid:  usr.Department != "",
+		},
+		Enabled:     usr.Enabled,
+		DateCreated: usr.DateCreated.UTC(),
+		DateUpdated: usr.DateUpdated.UTC(),
+	}
+}
+
+func toBusUser(dbUsr user) (userbus.User, error) {
+	addr := mail.Address{
+		Address: dbUsr.Email,
+	}
+
+	roles := make([]userbus.Role, len(dbUsr.Roles))
+	for i, value := range dbUsr.Roles {
+		var err error
+		roles[i], err = userbus.ParseRole(value)
+		if err != nil {
+			return userbus.User{}, fmt.Errorf("parse role: %w", err)
+		}
+	}
+
+	bus := userbus.User{
+		ID:           dbUsr.ID,
+		Name:         dbUsr.Name,
+		Email:        addr,
+		Roles:        roles,
+		PasswordHash: dbUsr.PasswordHash,
+		Enabled:      dbUsr.Enabled,
+		Department:   dbUsr.Department.String,
+		DateCreated:  dbUsr.DateCreated.In(time.Local),
+		DateUpdated:  dbUsr.DateUpdated.In(time.Local),
+	}
+
+	return bus, nil
+}
+
+func toBusUserSlice(dbUsers []user) ([]userbus.User, error) {
+	bus := make([]userbus.User, len(dbUsers))
+
+	for i, dbUsr := range dbUsers {
+		var err error
+		bus[i], err = toBusUser(dbUsr)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return bus, nil
 }
--- business/domain/userbus/stores/userdb/user.db.go
+++
@@ -1,2 +0,0 @@
-// Package userdb contains user related CRUD functionality.
-package userdb
